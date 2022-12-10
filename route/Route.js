2b604741e19cc400770fae206a54847d8fd492dc
Rayyan Alfawazy Adheswarna <adheswarna@gmail.com>
2022-12-10 22:04:13 +0700

    user Handling renew from scratch

--- route/Route.js
+++ route/Route.js
@@ -1,17 +1,10 @@
 import express from 'express'
-import { createUser, getUsers, loginUser } from '../controllers/UserController.js'
 import { getProductById, getProducts, createProduct, updateProduct, deleteProduct } from '../controllers/ProdukController.js'
 import { createSampah, getSampah, getSampahById, updateSampah, deleteSampah } from '../controllers/SampahController.js'
-import { verifyToken } from '../middleware/VerifyToken.js'
-import { refreshToken } from '../controllers/RefreshToken.js'
 
 const router = express.Router()
 
 // User Routing
-router.get('/users', verifyToken ,getUsers)
-router.post('/users/register', createUser)
-router.post('/users/login', loginUser)
-router.get('/users/token', refreshToken)
 
 // Produk Routing
 router.get('/products', getProducts)
